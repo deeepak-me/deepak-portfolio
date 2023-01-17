49438e766ca7da4f953471138c5e95c69c997ac9
deeepak-me <deepak.krishnan.e@gmail.com>
2023-01-18 00:32:16 +0530

    feat: navbar toggle operation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,50 @@
 import './Header.css'
+import {Home,Person, Message, List, Close, Menu} from '@mui/icons-material';
+
 
 export  const Header = () => {
+
+  const [toggle,setToggle] = useState(false);
+
   return (
     <header className='header'>
-      <nav className='nav container'>
+      <nav className="nav container">
         <a href="index.html" className="nav__logo">Dexpak</a>
 
-        <div className="nav__menu">
+        <div className={toggle ? "nav__menu nav__menu__active" :"nav__menu"}>
           <ul className="nav__list">
             <li className="nav__items">
               <a href="#home" className='nav__links'>
-                <i></i>Home
+                <div className="nav__icon"><Home/></div>Home
               </a>
             </li>
 
             <li className="nav__items">
               <a href="#about" className='nav__links'>
-                <i></i>About
+                <div className="nav__icon"><Person /></div>About
               </a>
             </li>
 
             <li className="nav__items">
               <a href="#skills" className='nav__links'>
-                <i></i>Skills
+              <div className="nav__icon"><List/></div>Skills
               </a>
             </li>
 
             <li className="nav__items">
               <a href="#contact" className='nav__links'>
-                <i></i>contact
+              <div className="nav__icon"><Message/></div>Contact
               </a>
             </li>
           </ul>
+        
+          <div className="nav__close" onClick={() => setToggle(!toggle)}>
+            <Close />
+          </div>
+          
+        </div>
+        <div className="nav__toggle" onClick={()=> setToggle(!toggle)}>
+          <Menu />
         </div>
       </nav>
     </header>
