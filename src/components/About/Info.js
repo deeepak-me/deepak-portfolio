bd531b23ed5966ff3c94e61f8437e7c071ee3469
deeepak-me <deepak.krishnan.e@gmail.com>
2023-01-19 00:16:42 +0530

    feat: About component additional information file

---
+++ src/components/About/Info.js
@@ -0,0 +1,20 @@
+
+export const Info = () => {
+  return (
+    <div className="about__info grid">
+
+      <div className="about__box">
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">1 years freelancing</span>
+      </div>
+   
+      <div className="about__box">
+        <h3 className="about__title">Education</h3>
+        <span className="about__subtitle">B.Tech in CSE</span>
+      </div>
+
+    </div>
+  )
+}
+
+// Completed Bachelor's  degree in Computer Science and Engineering from College Of engineering,Chengannur
