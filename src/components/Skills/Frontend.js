f2a84b152aa88f2101ac24be89293a0254e6e7f1
deeepak-me <deepak.krishnan.e@gmail.com>
2023-01-21 00:15:03 +0530

    feat: Skills list

---
+++ src/components/Skills/Frontend.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export const Frontend = () => {
+  return (
+    
+    <div className="skills__content">
+
+        <h3 className="skills__title">Frontend Developer</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">Language
+                <div className="skills__data">
+
+                    <div className="skills__name">HTML</div>
+                    <div className="skills__level">Basic</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">CSS</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">JavaScript</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">Python</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">C</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
