f8d6d76ce26e32c97d2dc44b49baea2edba5cf9d
deeepak-me <deepak.krishnan.e@gmail.com>
2023-01-23 00:47:05 +0530

    feat: Skills section

--- src/components/Skills/Frontend.js
+++ src/components/Skills/Frontend.js
@@ -40,6 +40,72 @@ export const Frontend = () => {
 
                 </div>
             </div>
+            <div className="skills__group">Frameworks
+                <div className="skills__data">
+
+                    <div className="skills__name">ReactJS</div>
+                    <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">VueJS</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">NextJS</div>
+                  <div className="skills__level">Basic</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">ExpressJS</div>
+                  <div className="skills__level">Basis</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">NodeJS</div>
+                  <div className="skills__level">Basic</div>
+
+                </div>
+            </div>
+            <div className="skills__group">Tools
+                <div className="skills__data">
+
+                    <div className="skills__name">MongoDB</div>
+                    <div className="skills__level">Basic</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">GiT & GitHub </div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">Bash</div>
+                  <div className="skills__level">Advanced</div>
+
+                </div>
+            </div>
+            <div className="skills__group">Design
+                <div className="skills__data">
+
+                    <div className="skills__name">Figma</div>
+                    <div className="skills__level">Basic</div>
+
+                </div>
+                <div className="skills__data">
+
+                  <div className="skills__name">WireFraming</div>
+                  <div className="skills__level">Basic</div>
+
+                </div>
+            </div>
         </div>
     </div>
 
