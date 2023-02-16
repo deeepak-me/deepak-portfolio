1fe92c6024c1b140a4c3fe562d81fb61b9ad87fa
deeepak-me <deepak.krishnan.e@gmail.com>
2023-02-16 20:49:01 +0530

    feat: Social media handles file completed

---
+++ src/components/Home/Social.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Instagram,GitHub,LinkedIn} from '@mui/icons-material';
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon"><Instagram /></a>
+        <a href="" className="home__social-icon"><GitHub /></a>
+        <a href="" className="home__social-icon"><LinkedIn /></a>
+    </div>
+  )
+}
