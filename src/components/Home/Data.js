4d4a1d652f71ec2b4a1c4f9bea7446b99f6dbecc
deeepak-me <deepak.krishnan.e@gmail.com>
2023-02-16 20:48:33 +0530

    feat: personal data file created

---
+++ src/components/Home/Data.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Data = () => {
+  return (
+    <div className="home-description">
+
+              <h3>hi, i am</h3>
+              <h1 className='home__title'>Deepak Krishnan E</h1>
+              <h2 className='home__subtitle'>FrontEnd Web Developer | Software Engineer</h2>
+
+              <a href="#contact" className="button button-flex">say hello</a>
+
+    </div>
+  )
+}
+
