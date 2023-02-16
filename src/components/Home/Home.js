c5e80ef86988c8a0859256b54264a886aca7f863
deeepak-me <deepak.krishnan.e@gmail.com>
2023-02-16 20:49:39 +0530

    feat: Home file major parts completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,40 @@
 import React from 'react'
 import "./Home.css";
 import hero from "../../assets/hero.jpeg"
+import {Social} from "./Social"
+import { Data } from './Data';
+import ScrollDown from './ScrollDown';
 
 export const Home = () => {
   return (
     
     <section className='home section' id="home">
 
-      <div className="content">
+      <div className="home__container container grid">
+          <div className="home__content grid">
+            <Social />
+            <div className="hero-img-wrap">
 
-        <div className="hero-img-wrap">
+              <div className="hero-img-container">
+                <img src={hero} alt="" className="hero-img" />
+              </div>
 
-          <div className="hero-img-container">
-            <img src={hero} alt="" className="hero-img" />
+              <div id="hero-img-shadow-1" className="hero-img-shadow-wrap">
+                <div className="hero-img-shadow"></div>
+              </div>
+
+              <div
+                id="hero-img-shadow-2"
+                className="hero-img-shadow-wrap hero-img-shadow-wrap--intro"
+              >
+                <div className="hero-img-shadow"></div>
+              </div>
+
+            </div>
+
+            < Data />
           </div>
-
-          <div id="hero-img-shadow-1" className="hero-img-shadow-wrap">
-            <div className="hero-img-shadow"></div>
-          </div>
-
-          <div
-            id="hero-img-shadow-2"
-            className="hero-img-shadow-wrap hero-img-shadow-wrap--intro"
-          >
-            <div className="hero-img-shadow"></div>
-          </div>
-
-        </div>
-
-        <div className="hero-description">
-
-          <h3>hi,</h3>
-          <h1>Deepak Krishnan E</h1>
-          <h2>FrontEnd Web Developer | Software Engineer</h2>
-
-        </div>
-
+          <ScrollDown />
       </div>
     </section>
 
